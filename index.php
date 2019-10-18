3c100731411fc0f25db9407bcf7ed4cc7d8ef805
Daniel Moster <22249073+dmoster@users.noreply.github.com>
2019-10-17 23:02:10 -0400

    add header/footer/index files

--- index.php
+++ index.php
@@ -1,5 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-echo 'Twas brillig';
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Press+Start+2P|Roboto" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+  <link rel="stylesheet" href="src/prove02.css">
 
-?>
+  <title>Home - Daniel Moster</title>
+</head>
+<body>
+  <?php include 'src/header.php'; ?>
+
+  <main>
+    <div class="content">
+      <div id="intro">
+        <p onmouseover="toggleColor(this)" onmouseout="toggleColor(this)" class="lead">I'm a developer, IT guy, mandolin player, writer, husband, and dad. Most of what I do with the first four are for the benefit of the last two.</p>
+
+        <div class="img-container">
+          <img src="images/me.jpg" alt="Handsome picture of me" width="250px">
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <article>
+    <div class="content">
+
+    </div>
+  </article>
+
+  <?php include 'src/footer.php'; ?>
+
+  <script src="src/main.js"></script>
+</body>
+</html>
