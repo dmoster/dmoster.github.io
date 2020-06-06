68539ff8d7a7f70091720e8cfbf0a56a7c2779bc
Daniel Moster <dmoster@Daniels-MacBook-Pro.local>
2020-06-06 13:38:49 -0400

    add wdd330 w07

---
+++ byui/wdd330/w07/quiz.js
@@ -0,0 +1,9 @@
+const quiz = [
+  { name: "Superman",realName: "Clark Kent" },
+  { name: "Wonderwoman",realName: "Dianna Prince" },
+  { name: "Batman",realName: "Bruce Wayne" },
+  { name: "The Hulk",realName: "Bruce Banner" },
+  { name: "Spider-man",realName: "Peter Parker" },
+  { name: "Cyclops",realName: "Scott Summers" }
+];
+
